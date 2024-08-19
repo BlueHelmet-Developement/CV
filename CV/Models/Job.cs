ab00445fc2c1dff6e72d6ce6091639dee03e3c95
Steffen Hvid Hansen <steffenhvid@gmail.com>
2024-08-19 20:11:02 +0200

    Updated the style

--- CV/Models/Job.cs
+++ CV/Models/Job.cs
@@ -3,7 +3,9 @@
     public class Job
     {
         public string? Start { get; set; }
+        public DateTime StartDate => DateTime.Parse(Start ?? "2000-01-01");
         public string? Slut { get; set; }
+        public DateTime SlutDate => DateTime.Parse(Slut ?? "2000-01-01");
         public List<string>? Titler { get; set; } = [];
         public string? Firma { get; set; }
         public List<string> TagLines { get; set; } = [];
